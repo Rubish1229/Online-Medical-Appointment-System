8ed4b25f330927ae4f112b2d0eea894dfa618447
Rubish1229 <karmacharyarubish5@gmail.com>
2025-11-14 17:36:10 +0545

    Addeed inputfield and buttons in login page

---
+++ navbar.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="home.css">
+    
+
+ <nav>
+        <div class="navbar">
+            <div class="logo">
+            <img src="./images/SWASTHYA.png" alt="logo"> </div>
+        
+        <div class="nav-list">
+            <ul>
+                <li><a href=""> &nbsp  Find <br>doctors</a> </li>
+                <li><a href=""> &nbsp &nbsp &nbsp Find <br>departments</a></li>
+                <li><a href=""> &nbsp&nbsp  Find <br>hospitals</a></li>
+                <li><a href=""> &nbsp Health <br>packages</a></li>
+                <li><a href="./part/login.html" class="i-login"> <i class="fa-regular fa-user"></i><p class="p-login">LOGIN</p></a></li>
+                <li><a href="" class="i-signup signup"> <i class="fa-solid fa-arrow-right-to-bracket" style="color: #0D6DFD;"></i><p class="p-signup">SIGN-UP</p></a></li>
+            
+
+            </ul>
+        </div>
+        </div>
+    </nav>
